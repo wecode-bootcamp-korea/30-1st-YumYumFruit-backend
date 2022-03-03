2985b76231d987d7c0bd05b617ea4f788148e280
lesol <wingnawing@gmail.com>
2022-03-03 15:21:14 +0900

    Create: products app >> ProductListView with pagination, sorting, filtering in GET method

--- products/views.py
+++ products/views.py
@@ -1,3 +1,52 @@
-from django.shortcuts import render
+import json, math
 
-# Create your views here.
+from django.http           import JsonResponse
+from django.views          import View
+from django.core.paginator import Paginator
+
+from products.models    import Product, ProductImage, Category
+from users.models       import User, ShoppingCart
+from orders.models      import OrderDetail
+
+class ProductListView(View):
+    def post(self, request):
+        try:
+            data     = json.loads(request.body)
+
+            sort     = data.get("sort", "-price")
+            category = data.get("category", "1")
+
+            product_list = list(Product.objects.filter(category_id=category).order_by(sort).values())
+            # paginator    = Paginator(product_list, 12)
+            # page = paginator.get_page(data.get("page", "1")) # paginator 사용해보고 싶어서 작업중이던 부분 일단 남겨둡니다
+
+            return JsonResponse({"message":"SUCCESS", "results":results}, status=200)
+        except KeyError:
+            return JsonResponse({"results":"KEY_ERROR"}, status=400)
+        except json.JSONDecodeError:
+            return JsonResponse({"results":"INVALID_DATA"}, status=400)
+
+
+    def get(self, request):
+        try:
+            sort     = request.GET.get("sort", "price") 
+            category = request.GET.get("category", "all") 
+
+            page      = int(request.GET.get("page", 1)) 
+            page_size = 12
+            limit     = int(page_size * page) 
+            offset    = int(limit - page_size)
+
+            if category != "all" :
+                product_offset = list(Product.objects.filter(category_id=category).order_by(sort)[offset:limit].values())
+                total_items    = Product.objects.filter(category_id=category).count()
+            else:
+                product_offset = list(Product.objects.all().order_by(sort)[offset:limit].values())
+                total_items    = Product.objects.all().count()
+
+            page_count     = total_items / page_size
+            return JsonResponse({"message":"SUCCESS", "results":product_offset, "total_pages":math.ceil(page_count), "total_items":total_items}, status=200)
+        except KeyError:
+            return JsonResponse({"results":"KEY_ERROR"}, status=400)
+        except json.JSONDecodeError:
+            return JsonResponse({"results":"INVALID_DATA"}, status=400)
