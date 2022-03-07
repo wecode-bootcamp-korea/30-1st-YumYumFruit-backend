607fac2714fc6c7676d7728e84c493354cd33f82
park geon woo <parkgeonwoo@parks-MacBook-Air.local>
2022-03-07 19:43:05 +0900

    modify:decorator,token,login

--- users/utils.py
+++ users/utils.py
@@ -4,18 +4,21 @@ import jwt
 from django.http  import JsonResponse
 
 from users.models import User
-from my_settings  import SECRET_KEY, AIGORHM
+from my_settings  import SECRET_KEY, AIGORITHM
 
 def login_decorator(func):
     def wrapper(self, request, *args, **kwargs):
         try:
-            token = request.headers.get('Authorization')          
-            payload = jwt.decode(token, SECRET_KEY, AIGORHM)  
-            user = User.objects.get(id=payload['id'])                 
+            token        = request.headers.get('Authorization')          
+            payload      = jwt.decode(token,SECRET_KEY,AIGORITHM)  
+            user         = User.objects.get(id=payload['id'])                 
             request.user = user                                                
-
+        
+        except jwt.exceptions.ExpiredSignatureError:
+            return JsonResponse({"message" : "EXPIRED_TOKEN"}, status=401)
+        
         except jwt.exceptions.DecodeError:                                     
-            return JsonResponse({'message' : 'INVALID_TOKEN' }, status=400)
+            return JsonResponse({'message' : 'INVALID_TOKEN'}, status=400)
 
         except User.DoesNotExist:                                           
             return JsonResponse({'message' : 'INVALID_USER'}, status=400)
--- users/views.py
+++ users/views.py
@@ -2,32 +2,31 @@ import json
 import bcrypt
 import jwt
 
-from django.http  import JsonResponse
+from django.http  import HttpResponseBadRequest, JsonResponse
 from django.views import View
+from datetime     import datetime, timedelta
 
 from users.models    import User, ShoppingCart
 from users.validator import validate_email, validate_password
-from my_settings  import SECRET_KEY, AIGORHM
-from users.utils import login_decorator
-from products.models import Product
+from my_settings     import SECRET_KEY, AIGORITHM
 
 class SignupView(View):
     def post(self, request):
         try:
             data           = json.loads(request.body)
-            username       = data['username']
+            name           = data['name']
             email          = data['email']
             password       = data['password']
             phone_number   = data['phone_number']   
             
             if not validate_email(email):
-                   return JsonResponse({"message" : "INVAILD EMAIL"}, status = 400)
+                   return JsonResponse({"message" : "INVAILD_EMAIL"}, status = 400)
             
             if not validate_password(password):
-                       return JsonResponse({"message" : "INVALID PASSWORD"}, status = 400)
+                       return JsonResponse({"message" : "INVALID_PASSWORD"}, status = 400)
             
             if User.objects.filter(email = email).exists():
-                return JsonResponse({"message":"DUPLICATE_EMAIL"}, status=400)
+                return JsonResponse({"message" : "DUPLICATE_EMAIL"}, status=400)
             
             new_salt = bcrypt.gensalt()
             new_password = password.encode('utf-8')
@@ -35,14 +34,14 @@ class SignupView(View):
             decode_password = hashed_password.decode('utf-8')
              
             User.objects.create(
-                name         = username,
+                name         = name,
                 email        = email,
                 password     = decode_password,
                 phone_number = phone_number,
             )
-            return JsonResponse({"message":"SUCCESS"},status=201)
+            return JsonResponse({"message" : "SUCCESS"},status=201)
         except KeyError:
-            return JsonResponse({"message":"KEY_ERROR"},status=400)
+            return JsonResponse({"message" : "KEY_ERROR"},status=400)
 
 class SigninView(View):
       def post(self, request):
@@ -51,16 +50,19 @@ class SigninView(View):
             email    = data["email"]
             password = data["password"]
                     
-            if not User.objects.filter(email = email).exists():
-                return JsonResponse({"message":"INVALID_USER"},status=401)
+            if not User.objects.filter(email=email).exists():
+              return HttpResponseBadRequest(status=404)
             
             user            = User.objects.get(email=email)
             hashed_password = user.password.encode('utf-8') 
-          
-            if bcrypt.checkpw(password.encode('utf-8'),hashed_password):
-                token = jwt.encode({'id': user.id},SECRET_KEY,AIGORHM)
-                return JsonResponse({"token":token},status=200)
             
-            return JsonResponse({"message": "INVAILD_USER"}, status=401)  
+            if not bcrypt.checkpw(password.encode('utf-8'),hashed_password):
+                return JsonResponse({"message" : "INVAILD_USER"}, status=401) 
+            
+            token = jwt.encode({'id': user.id, 'exp':datetime.utcnow() + timedelta(days=2)},SECRET_KEY,AIGORITHM)
+            
+             
+            return JsonResponse({"token" : token},status=200)
         except KeyError:
-            return JsonResponse({"message": "KEY_ERROR"}, status=400) 
+            return JsonResponse({"message" : "KEY_ERROR"}, status=400) 
+
