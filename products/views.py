58d82eb04743569cb5cf8a08baf1c98413863dc1
lesol <wingnawing@gmail.com>
2022-03-07 19:55:43 +0900

    Edit: products/views.py >> Review 수정 완료 -변수명, urls, Q객체

--- products/views.py
+++ products/views.py
@@ -2,6 +2,7 @@ import json, math
 
 from django.http           import JsonResponse
 from django.views          import View
+from django.db.models      import Q
 
 from products.models    import Product, ProductImage, Category
 from users.models       import User, ShoppingCart
@@ -10,31 +11,31 @@ from orders.models      import OrderDetail
 class ProductListView(View):
     def get(self, request):
         try:
-            sort      = request.GET.get("sort", "price")
-            category  = request.GET.get("category", "all")
-            page      = int(request.GET.get("page", 1))
-            page_size = 12
-            limit     = int(page_size * page)
-            offset    = int(limit - page_size)
+            sort        = request.GET.get("sort", "price")
+            category_id = request.GET.get("category", "all")
+            page        = int(request.GET.get("page", 1))
+            page_size   = 12
+            limit       = int(page_size * page)
+            offset      = int(limit - page_size)
 
-            if category != "all" :
-                product_list   = Product.objects.filter(category_id=category)
-            else:
-                product_list   = Product.objects.all()
+            category_validator = [1,2,3,4,"all"]
 
-            total_items    = product_list.count()
-            page_count     = math.ceil(total_items / page_size)
+            if category_id not in category_validator:
+                return JsonResponse({"results":"INVALID_PARAMETER"}, status=400)
+
+            q = Q()
+            if category_id != "all" :
+                q &= Q(category_id=category_id)
+                
+            product_list   = Product.objects.filter(q)
+            total_count    = product_list.count()
+            page_count     = math.ceil(total_count / page_size)
             product_offset = list(product_list.order_by(sort)[offset:limit].values())
 
             return JsonResponse({
-                                    "message"    :"SUCCESS",
-                                    "results"    :product_offset,
-                                    "total_pages":page_count,
-                                    "total_items":total_items
-                                }, status=200)
+                "product_offset"       :product_offset,
+                "total_number_of_pages":page_count,
+                "total_count"          :total_count
+                }, status=200)
         except KeyError:
             return JsonResponse({"results":"KEY_ERROR"}, status=400)
-        except json.JSONDecodeError:
-            return JsonResponse({"results":"INVALID_DATA"}, status=400)
-        except ValueError:
-            return JsonResponse({"results":"INVALID_PARAMETER"}, status=400)
