abcd97f40b35c1e64485a7cf033ff80030b7317f
park geon woo <parkgeonwoo@parks-MacBook-Air.local>
2022-03-09 18:16:05 +0900

    add:wishlist_crud

--- users/views.py
+++ users/views.py
@@ -1,13 +1,15 @@
 import json
+
 import bcrypt
 import jwt
 
-from django.http  import HttpResponseBadRequest, JsonResponse
+from django.http  import HttpResponse, HttpResponseBadRequest, JsonResponse
 from django.views import View
 from datetime     import datetime, timedelta
 
-from users.models    import User, ShoppingCart
+from users.models    import User, Wishlist
 from users.validator import validate_email, validate_password
+from users.utils     import login_decorator
 from my_settings     import SECRET_KEY, ALGORITHM
 
 class SignupView(View):
@@ -20,10 +22,10 @@ class SignupView(View):
             phone_number   = data['phone_number']   
             
             if not validate_email(email):
-                   return JsonResponse({"message" : "INVAILD_EMAIL"}, status = 400)
+                return JsonResponse({"message" : "INVAILD_EMAIL"}, status = 400)
             
             if not validate_password(password):
-                       return JsonResponse({"message" : "INVALID_PASSWORD"}, status = 400)
+                return JsonResponse({"message" : "INVALID_PASSWORD"}, status = 400)
             
             if User.objects.filter(email = email).exists():
                 return JsonResponse({"message" : "DUPLICATE_EMAIL"}, status=400)
@@ -66,3 +68,73 @@ class SigninView(View):
         except KeyError:
             return JsonResponse({"message" : "KEY_ERROR"}, status=400) 
 
+class WishlistView(View):
+    @login_decorator
+    def post(self, request):
+        try:
+            data         = json.loads(request.body)
+            user         = request.user  
+            wish_product = data["product_id"]
+        
+            if Wishlist.objects.filter(user_id=user.id,product_id=wish_product).exists():
+              return JsonResponse({"message" : "DUPLICATE_PRODUCT"})
+            else:
+                Wishlist.objects.create(user_id=user.id,product_id=wish_product)
+                return JsonResponse({"message" : "WISHLIST_CREATED"},status=201)
+        
+        except Wishlist.DoesNotExist:
+            return JsonResponse({"message" : "INVALID_CART"}, status=400)
+        except KeyError:
+            return JsonResponse({'message' : 'KEY_ERROR'}, status = 400)
+   
+    @login_decorator   
+    def get(self,request):
+        try:
+            user      = request.user
+            wishlists = Wishlist.objects.filter(user_id=user.id)
+              
+            result = [
+                {
+                    'user_id'                 : user.id,
+                    'product_id'              : wishlist.product.id,
+                    'name'                    : wishlist.product.name,
+                    'price'                   : int(wishlist.product.price),
+                    'thumbnail_image_url'     : wishlist.product.thumbnail_image_url,
+                } for wishlist in wishlists
+            ]
+            return JsonResponse({"result":result},status=200)
+            
+        except Wishlist.DoesNotExist:
+            return JsonResponse({'message':'DOES_NOT_EXITST_WISHLIST'}, status = 400)
+        except KeyError:
+            return JsonResponse({"message" : "KEY_ERROR"}, status=400)
+    
+    # @login_decorator
+    # def delete(self,request,product_id):
+    #     try:
+    #         user = request.user  
+    #         Wishlist.objects.filter(user_id=user.id,product_id=product_id).delete()
+            
+    #         return JsonResponse({'result':'DELETE_SELECT PRODUCT'}, status = 200)
+        
+    #     except KeyError:
+    #          return JsonResponse({"message" : "KEY_ERROR"}, status=400)
+    
+    @login_decorator
+    def delete(self,request):
+        try:
+            user     = request.user 
+            wishlists = request.GET.get("wishlist_id").split(",")
+            
+            for wishlist in wishlists:
+                
+                if wishlist == "0":
+                    Wishlist.objects.filter(user_id=user.id).all().delete()
+                   
+                
+                Wishlist.objects.filter(user_id=user.id,id=wishlist).delete()
+            
+            return JsonResponse({"message":"DELETE_SUCCESS"},status=204)
+        except KeyError:
+            return JsonResponse({"message" : "KEY_ERROR"}, status=400)
+    
